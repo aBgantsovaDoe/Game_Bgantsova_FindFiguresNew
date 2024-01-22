9cea94cf70be132d9251ccb34b28313a03b51b5f
abgantsova <abgantsova@hr-link.ru>
2024-01-22 15:17:52 +0300

    Ненужные файлы

--- data.js
+++
@@ -1,8 +0,0 @@
- const USERS = {
-       first: 10,
-   second: 30,
-   third: 15,
-   forth: 2
- };
- let jsonUsers = JSON.stringify(USERS);
- localStorage.setItem('users', jsonUsers);
--- util.js
+++
@@ -1,9 +0,0 @@
-const getCurrentFromStorage = function () {
-    return JSON.parse(localStorage.getItem('user'));
-  }
-  
-  const putCurrentInStorage = function (user) {
-    localStorage.setItem('user', JSON.stringify(user));
-  };
-  
-  export {putCurrentInStorage, getCurrentFromStorage};
